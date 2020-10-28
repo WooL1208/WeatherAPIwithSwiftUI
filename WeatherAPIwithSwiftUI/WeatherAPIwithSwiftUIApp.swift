4f636895013f3c8074fe5dfde7b4c4ecc95dfedb
WooL1208 <58040607+WooL1208@users.noreply.github.com>
2020-10-28 16:42:29 +0800

    Initial Commit

---
+++ WeatherAPIwithSwiftUI/WeatherAPIwithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIwithSwiftUIApp.swift
+//  WeatherAPIwithSwiftUI
+//
+//  Created by WooL on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIwithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
